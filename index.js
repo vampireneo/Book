0ca6f0964dc679f64e0f3036573ae3c4daaf1fc1
VampireNeo <neo.choi@gmail.com>
2015-04-23 01:49:49 +0800

    for 誠品, search data from desktop version instead of mobile version

--- index.js
+++ index.js
@@ -166,23 +166,52 @@ function getFromBooks(pISBN) {
 }
 
 function getFromEslite(pISBN) {
-	var domain = "https://mssl.eslite.com";
-	var searchUrl = "/main/GetSearchProduct/";
-	var productUrl = "/main/product/";
+	var domain = "http://www.eslite.com/";
+	var searchUrl = "Search_BW.aspx?query=";
 	var bookObj = {};
 
-	request({url:domain + searchUrl + pISBN + ",10,1", tunnel: true}, function (error, response, body) {
+	request({url:domain + searchUrl + pISBN, tunnel: true}, function (error, response, body) {
 		if (!error) {
-			var info = JSON.parse(body);
-			var productId = info[0].SearchItem[0].ProductID;
-				
-			request(domain + productUrl + productId, function (error, response, body) {
+			var $ = cheerio.load(body);
+			var link = $("#search_content td.name h3 a").eq(0);
+			var targetUrl = $(link).attr("href");
+
+			request(domain + targetUrl, function (error, response, body) {
 				if (!error) {
 					var $ = cheerio.load(body);
-					bookObj.Title = $("section.View .BookInfor h3").text().trim();
-					bookObj.ImageUrl = $(".BookViewList li .BookImg img").attr("src");
-					bookObj.Author = $("section.View .BookInfor .Author b").text().trim()
-					bookObj.Publisher = $("section.View .BookInfor .Publish b").text().trim()
+					bookObj.Title = $("#content h1 span").eq(0).text().trim();
+					bookObj.SubTitle = $("#content h1 span").eq(1).text().trim();
+					bookObj.ImageUrl = $("#mainlink img").attr("src");
+					var infos = $("#content h3");
+					infos.each(function(i, info) {
+						var text = $(info).text();
+						if (text.indexOf("作者") != -1) {
+							bookObj.Author = $("a", info).eq(0).text().trim();
+						} else if (text.indexOf("譯者") != -1) {
+							bookObj.Translater = $("a", info).eq(0).text().trim();
+						} else if (text.indexOf("出版社") != -1) {
+							bookObj.Publisher = $("a", info).eq(0).text().trim();
+						} else if (text.indexOf("出版日期") != -1) {
+							bookObj.PublishDate = text.split("／")[1].trim();
+						} else if (text.indexOf("商品語言") != -1) {
+							bookObj.Language = text.split("／")[1].trim();
+						} else if (text.indexOf("裝訂") != -1) {
+							bookObj.Spec = text.split("／")[1].trim();
+						}
+					});
+					infos = $("#content .C_box p").text().split("\n");
+					for(var i = 0; i < infos.length; i++) {
+						if (infos[i].indexOf("ISBN 13") > -1) {
+							bookObj.ISBN = infos[i].split("／")[1].trim();
+						}
+					}
+					infos = $("#content .C_box table[id*='dlSpec'] td");
+					infos.each(function(i, info) {
+						var text = $("span",info).eq(0).text();
+						if (text.indexOf("頁數") != -1) {
+							bookObj.Pages = $("span",info).eq(1).text();
+						}
+					});
 
 					console.log(bookObj);
 				} else {
