d4bca121a29cdc3f6aacdc2d171ba8997b744fbd
VampireNeo <neo.choi@gmail.com>
2015-04-24 01:21:40 +0800

    able to get info from 商務

--- index.js
+++ index.js
@@ -228,9 +228,60 @@ function getFromJointPublishing(pISBN) {
 	});
 }
 
+function getFromCommercialPress(pISBN) {
+	var searchUrl = "http://www.cp1897.com.hk/product_info.php?BookId=";
+	var bookObj = {};
+
+	request(searchUrl + pISBN, function (error, response, body) {
+		if (!error) {
+			var $ = cheerio.load(body);
+
+			bookObj.Title = $("#mainPanel #rightPanel #productInfo_table td.productName").text().replace(/[《》]/gi,"");
+			var infos = $("#mainPanel #rightPanel #productInfo_table tr");
+			infos.each(function(i, info) {
+				var text = $("td.productLabel", info).text();
+				var value = $("td.productDesc", info).text().trim();
+				if (text.indexOf("作者") != -1) {
+					bookObj.Author = value;
+				} else if (text.indexOf("譯者") != -1) {
+					bookObj.Translater = value;
+				} else if (text.indexOf("出版社") != -1) {
+					bookObj.Publisher = value;
+				} else if (text.indexOf("ISBN") != -1) {
+					bookObj.ISBN = value;
+				}
+			});
+
+			var infos = $("#mainPanel #rightPanel #extraInfo_table tr");
+			infos.each(function(i, info) {
+				var text = $("td.productLabel", info).text();
+				var value = $("td.productDesc", info).text().trim();
+				if (text.indexOf("出版日期") != -1) {
+					bookObj.PublishDate = value;
+				} else if (text.indexOf("語言版本") != -1) {
+					bookObj.Language = value;
+				} else if (text.indexOf("頁數") != -1) {
+					bookObj.Pages = value;
+				} else if (text.indexOf("裝幀") != -1) {
+					bookObj.Spec = value;
+				} else if (text.indexOf("叢書/系列") != -1) {
+					bookObj.Series = value;
+				}
+			});
+
+			console.log(bookObj);
+		} else {
+			console.log("We’ve encountered an error: " + error);
+		}
+	});
+}
+
 pISBN = process.argv[2] || pISBN;
 
+/*
+*/
 getFromKingstone(pISBN);
 getFromBooks(pISBN);
 getFromEslite(pISBN);
 getFromJointPublishing(pISBN);
+getFromCommercialPress(pISBN);
