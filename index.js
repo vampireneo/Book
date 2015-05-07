17f8d5b8d21d2a29d21329cb4ed03a0ca27545b6
unknown <neo.choi@pc1465.inchcape.hk>
2015-05-07 13:58:41 +0800

    do some enhancement

--- index.js
+++ index.js
@@ -349,8 +349,20 @@ app.get('/isbn/:id([0-9]+)', function(req, res){
   //res.send('hello world');
 });
 
-app.listen(portNo);
-console.log("Listening port " + portNo);
+app.get('/isbn/', function(req, res){
+  res.redirect('/isbn/' + pISBN);
+});
 
-/*
-*/
+app.get('/', function (req, res) {
+  //res.send('Hello World!');
+  res.redirect('/isbn/' + pISBN);
+});
+
+var server = app.listen(portNo, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Listening at http://%s:%s', host, port);
+
+});
