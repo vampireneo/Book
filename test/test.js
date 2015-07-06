d478ca96924de323239d2b15a07d42aee7366346
Neo <neo.choi@gmail.com>
2015-07-07 01:36:41 +0800

    more test cases for server

--- test/test.js
+++ test/test.js
@@ -88,16 +88,33 @@ describe('Server', function(){
 			});
 		});
 
-	it('returns default ISBN should be 9789571358512', function(done) {
-		superagent.get('http://localhost:3000/').end(function(err, res) {
-			assert.ifError(err);
-			assert.equal(res.status, status.OK);
-			var result = JSON.parse(res.text);
-			//assert.deepEqual({ user: 'test' }, result);
-			assert.equal("9789571358512", result.ISBN[0]);
-			done();
+		it('returns should returns result of 9789571358512 by default', function(done) {
+			superagent.get('http://localhost:3000/').end(function(err, res) {
+				assert.ifError(err);
+				assert.equal(res.status, status.OK);
+				var result = JSON.parse(res.text);
+				//assert.deepEqual({ user: 'test' }, result);
+				assert.equal("9789571358512", result.ISBN[0]);
+				done();
+			});
 		});
-	});
 
+		it('returns default ISBN should be 9789571358512', function(done) {
+			superagent.get('http://localhost:3000/isbn/').end(function(err, res) {
+				assert.ifError(err);
+				assert.equal(res.status, status.OK);
+				var result = JSON.parse(res.text);
+				//assert.deepEqual({ user: 'test' }, result);
+				assert.equal("9789571358512", result.ISBN[0]);
+				done();
+			});
+		});
+
+		it('returns 404 if incorrect url', function(done) {
+			superagent.get('http://localhost:3000/isbbn/').end(function(err, res) {
+				assert.equal(res.status, status.NOT_FOUND);
+				done();
+			});
+		});
 	});
 });
